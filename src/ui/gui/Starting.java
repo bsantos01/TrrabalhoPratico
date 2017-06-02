67a860f144ec4d83354e534f37a0b989de4bda17
miguelaf24 <miguelaf24@gmail.com>
2017-06-02 17:25:59 +0100

    Starting V0.1.1

--- src/ui/gui/Starting.java
+++ src/ui/gui/Starting.java
@@ -48,28 +48,14 @@ public class Starting extends JPanel implements Observer {
 
     }
 
-
-
     private void setupComponents() {
         cbd = new JComboBox(sdif);
         cba = new JComboBox(sarea);
-        Welcome = new JLabel("Mini Rogue");
+        Welcome = new JLabel("Mini Rogue",SwingConstants.CENTER);
         DifLabel = new JLabel("Difficulty:");
         AreaLabel = new JLabel("Area:");
-        setDif = new JTextField("1");
-        setArea = new JTextField("1");
         start = new JButton("START");
-        
-        start.setAlignmentY(Component.CENTER_ALIGNMENT);
-        start.setAlignmentX(Component.CENTER_ALIGNMENT);
-        AreaLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        DifLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        setArea.setAlignmentX(Component.CENTER_ALIGNMENT);
-        setDif.setAlignmentX(Component.CENTER_ALIGNMENT);
-        setArea.setMaximumSize(new Dimension( 200, 24 ) );
-        setDif.setMaximumSize(new Dimension( 200, 24 ) );
         start.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent ae) {
                 int dif= Integer.parseInt(cbd.getSelectedItem().toString());
@@ -85,34 +71,34 @@ public class Starting extends JPanel implements Observer {
     }
     
     private void setupLayout() {
-        //setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
         if (game.getState() instanceof IBeginning) {    
             
-            JPanel pNorth, pCenter, outerp;
+            JPanel pNorth, outerp, pCenter;
             outerp= new JPanel(new BorderLayout());
             pNorth = new JPanel(new BorderLayout());
-            //pCenter = new JPanel(new BorderLayout());
-            
-            outerp.setPreferredSize(new Dimension(500,100));
+            pCenter = new JPanel(new BorderLayout());
+            add(outerp);
+            outerp.add(pNorth, BorderLayout.NORTH);
+            outerp.add(pCenter);
+            //outerp.setPreferredSize(new Dimension(500,100));
             pNorth.add(Welcome, BorderLayout.CENTER);
             
             Box right = Box.createVerticalBox();
-            right.add(Box.createHorizontalGlue());
-            right.add(AreaLabel, BorderLayout.CENTER);
-            right.add(Box.createVerticalGlue());
-            right.add(cba, BorderLayout.CENTER);
-             right.add(Box.createVerticalGlue());
+            right.add(AreaLabel, BorderLayout.EAST);
+            right.add(cba, BorderLayout.EAST);
             
             Box left = Box.createVerticalBox();
-            left.add(DifLabel, BorderLayout.CENTER);
-            left.add(cbd, BorderLayout.CENTER);
+            left.add(DifLabel, BorderLayout.WEST);
+            left.add(cbd, BorderLayout.WEST);
             
-            outerp.add(right, BorderLayout.EAST);
-            outerp.add(left, BorderLayout.WEST);
-            add(pNorth, BorderLayout.NORTH);
+            
+            pCenter.add(right, BorderLayout.CENTER);
+            pCenter.add(left, BorderLayout.EAST);
+            
             //outerp.add(pCenter, BorderLayout.CENTER);
-            add(outerp, BorderLayout.SOUTH);
+            
         }
 
         
