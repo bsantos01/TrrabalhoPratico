803d8f4564bcf7434c7569185aa3c84192027db7
miguelaf24 <miguelaf24@gmail.com>
2017-06-08 15:26:40 +0100

    DugPan-Vida Monstro!!! :ghost: :goat::dash:

--- src/ui/gui/DugeonPanel.java
+++ src/ui/gui/DugeonPanel.java
@@ -20,6 +20,7 @@ import trabalhopratico.Data.ObservableGame;
 class DugeonPanel extends JPanel{
 
    static private BufferedImage dugeon= null;
+   static private BufferedImage button= null;
     
     ObservableGame game;
     public static BufferedImage getdugeon(){
@@ -29,6 +30,7 @@ class DugeonPanel extends JPanel{
     static{
         try{
             dugeon=ImageIO.read(Resources.getResourceFile("Images/dugeon.png"));
+            button=ImageIO.read(Resources.getResourceFile("Images/button.png"));
         } catch(IOException e){
             System.out.println("ERROR LOADING IMAGE");
         }
@@ -42,7 +44,22 @@ class DugeonPanel extends JPanel{
     @Override
     public void paintComponent(Graphics g){
         g.drawImage(getdugeon(), 0, 0, getWidth() - 1, getHeight() - 1, null);
-    
+        int a = game.getMonsteHP();
+        int x1=0;
+        int y1=a;
+        if(a>10){
+            x1++;
+            y1-=10;
+        }
+        if(a>20){
+            x1++;
+            y1-=10;
+        }
+        int x = (int) (getWidth()*(0.88-x1*0.12));
+        int y = (int) (getHeight()*(0.91-y1*0.08));
+        g.drawImage(button, x,  y, (int) (getWidth() - (getWidth()*0.95)), (int) (getHeight() - (getHeight()*0.95)), null);
+        
+        
     }
     
 }
